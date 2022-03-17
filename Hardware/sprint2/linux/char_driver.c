ae2fb448ed8b28f0deb353f5f36e9f11a5e5cf12
xuwt <xu_wanting2017@163.com>
2022-03-17 10:18:29 -0700

    kbd/mode2 tests

--- Hardware/sprint2/linux/char_driver.c
+++ Hardware/sprint2/linux/char_driver.c
@@ -1,4 +1,68 @@
-void char_driver(int x1, int y1, int x2, int y2)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include "address_map_arm.h"
+
+/* Prototypes for functions used to access physical memory addresses */
+int open_physical (int);
+void * map_physical (int, unsigned int, unsigned int);
+void close_physical (int);
+int unmap_physical (void *, unsigned int);
+
+// Open /dev/mem, if not already done, to give access to physical addresses
+int open_physical (int fd)
+{
+   if (fd == -1)
+      if ((fd = open( "/dev/mem", (O_RDWR | O_SYNC))) == -1)
+      {
+         printf ("ERROR: could not open \"/dev/mem\"...\n");
+         return (-1);
+      }
+   return fd;
+}
+
+// Close /dev/mem to give access to physical addresses
+void close_physical (int fd)
+{
+   close (fd);
+}
+
+/*
+ * Establish a virtual address mapping for the physical addresses starting at base, and
+ * extending by span bytes.
+ */
+void* map_physical(int fd, unsigned int base, unsigned int span)
+{
+   void *virtual_base;
+
+   // Get a mapping from physical addresses to virtual addresses
+   virtual_base = mmap (NULL, span, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, base);
+   if (virtual_base == MAP_FAILED)
+   {
+      printf ("ERROR: mmap() failed...\n");
+      close (fd);
+      return (NULL);
+   }
+   return virtual_base;
+}
+
+/*
+ * Close the previously-opened virtual address mapping
+ */
+int unmap_physical(void * virtual_base, unsigned int span)
+{
+   if (munmap (virtual_base, span) != 0)
+   {
+      printf ("ERROR: munmap() failed...\n");
+      return (-1);
+   }
+   return 0;
+}
+
+int char_driver(int x1, int y1, int x2, int y2)
 {
    volatile int * point1_ptr;   // virtual address pointer to obtain x1,y1
    volatile int * point2_ptr;   // virtual address pointer to obtain x2,y2
@@ -21,5 +85,19 @@ void char_driver(int x1, int y1, int x2, int y2)
     
    unmap_physical (LW_virtual, LW_BRIDGE_SPAN);   // release the physical-memory mapping
    close_physical (fd);   // close /dev/mem
+   return 0;
+}
+
+int main(void)
+{
+   int x1,x2,y1,y2;
+   for(int i = 0; i<200; i++){
+      x1 = i;
+      y1 = i;
+      x2 = 200-i;
+      y2 = 200-i;
+      char_driver(x1, y1, x2, y2);
+      for(int j = 0; j<1000000; j++){}
+   }
    return 0;
 }
--- Hardware/sprint2/linux/usb_kbd.c
+++ Hardware/sprint2/linux/usb_kbd.c
@@ -13,6 +13,7 @@
 
 #define KBD_EVENT "/dev/input/event0"  
 
+
 int listen_kbd(int* kbd_ptr){
     int fd = -1;
     int ret = -1;
@@ -33,9 +34,11 @@ int listen_kbd(int* kbd_ptr){
     ret = select(fd + 1, &event_set, NULL, NULL, &timeout);
 
     if(ret < 0){
-        printf("Waiting Error\n"); 
+        //printf("Waiting Error\n"); 
+        *kbd_ptr = 0;
     }else if(ret == 0){
-        printf("Timeout\n");
+        //printf("Timeout\n");
+        *kbd_ptr = 0;
     }else{
         kbd_event = malloc(sizeof(struct input_event));  
         memset(kbd_event, 0, sizeof(struct input_event));  //clear/reset kbd_event
@@ -45,10 +48,12 @@ int listen_kbd(int* kbd_ptr){
         }
         if(kbd_event->type == 0x01){
         *kbd_ptr = kbd_event->code;
+        }else if(kbd_event->type == 0x04){
+        *kbd_ptr = kbd_event->value;
         }
-        printf("type: 0x%x\n ", kbd_event->type);  
-        printf("code: 0x%x\n ", kbd_event->code);  
-        printf("value: 0x%x\n", kbd_event->value); 
+        // printf("type: 0x%x\n ", kbd_event->type);  
+        // printf("code: 0x%x\n ", kbd_event->code);  
+        // printf("value: 0x%x\n", kbd_event->value); 
         free(kbd_event);
     }
     close(fd);
@@ -69,3 +74,31 @@ int listen_kbd(int* kbd_ptr){
     //     printf("value: 0x%x\n", kbd_event->value); 
     //     free(kbd_event);
     // }
+
+int main(void)
+{
+   int kbd_ptr;
+   int ret;
+   for(int i = 0; i<50; i++){
+       ret = listen_kbd(&kbd_ptr);
+       if(ret!=0){
+        if(kbd_ptr == 0x7001a || kbd_ptr == 0x11){
+            printf("recieved key: W\n");
+        }else if(kbd_ptr == 0x70004 || kbd_ptr == 0x1e){
+            printf("recieved key: A\n");
+        }else if(kbd_ptr == 0x70016 || kbd_ptr == 0x1f){
+            printf("recieved key: S\n");
+        }else if(kbd_ptr == 0x70007 || kbd_ptr == 0x20){
+            printf("recieved key: D\n");
+        }
+       }
+   }
+
+   return 0;
+}
+
+//W 7001a or 11
+//A 70004 or 1e
+//S 70016 or 1f
+//D 70007 or 20
+// space and all numbers will be added
